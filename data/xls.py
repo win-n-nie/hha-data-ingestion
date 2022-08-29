7b231b77c622178c05dbfada8c061c7bfd3cc4ee
win-n-nie <102179077+win-n-nie@users.noreply.github.com>
2022-08-29 00:57:49 -0400

    update 2

--- data/xls.py
+++ data/xls.py
@@ -1,2 +1,13 @@
 import pandas as pd
 import xlrd
+
+df = pd.read_excel('data/dataset.xlsx')
+df
+xls = xlrd.open_workbook('data/dataset.xlsx', on_demand=True)
+xls.sheet_names()
+
+df = pd.read_excel('data/dataset.xlsx', sheet_name='tab1')
+df
+
+df = pd.read_excel('data/dataset.xlsx', sheet_name='tab2')
+df
